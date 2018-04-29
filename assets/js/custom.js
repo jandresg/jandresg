314bb9f1a4962606cd86fe0f7e79e8fe4af5b88f
jandresg <jandrresg@gmail.com>
2018-04-29 13:45:06 -0400

    minned the main html, custome css & js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,6 @@
-// Initialize tooltip component
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
-
-// Initialize popover component
 $(function () {
   $('[data-toggle="popover"]').popover()
 })
